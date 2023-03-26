054cf19f0db88924632bb06154c6b0bd223d4aea
Laura <laura.vidal@internaliagroup.com>
2023-03-26 18:17:35 +0200

    api - character detail

--- 06-api/00-boilerplate/src/pods/character/api/character.api.ts
+++ 06-api/00-boilerplate/src/pods/character/api/character.api.ts
@@ -1,9 +1,14 @@
 import { Character } from './character.api-model';
 import { Lookup } from 'common/models';
 import { mockCities, mockCharacterCollection } from './character.mock-data';
+import axios from 'axios';
+
+const url = 'https://rickandmortyapi.com/api/character';
 
 export const getCharacter = async (id: string): Promise<Character> => {
-  return mockCharacterCollection.find((h) => h.id === id);
+  //return mockCharacterCollection.find((h) => h.id === id);
+  const { data } = await axios.get<Character>(`${url}/${id}`);
+  return data;
 };
 
 export const getCities = async (): Promise<Lookup[]> => {
