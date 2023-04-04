05cdb68db78150b2e3a4e84d36ccaa4e0d57d226
Laura <laura.vidal@internaliagroup.com>
2023-04-04 22:07:00 +0200

    react: rick y morty - debounce

--- 04-frameworks/react/src/character-search.tsx
+++ 04-frameworks/react/src/character-search.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { TextField } from "@mui/material";
-import Button from '@mui/material/Button';
-import SearchIcon from '@mui/icons-material/Search';
+import { useDebounce } from "use-debounce";
 
 interface Props {
     onSearch: (character: string) => void;
@@ -10,17 +9,16 @@ interface Props {
 export const CharacterSearch: React.FC<Props> = (props) => {
     const { onSearch } = props;
     const [ character, setCharacter ] = React.useState("");
+    const [debounceCharacter] = useDebounce(character,1000);
 
     React.useEffect(() => {
         onSearch(character);
-    }, []);
+    }, [debounceCharacter]);
 
     return (<>
-        <TextField id="outlined-basic" label="Character Name" variant="outlined" size="small"
+        <TextField id="outlined-basic" label="Search by Character Name" variant="outlined" size="small"
                     value={character}
                     onChange={(e) =>setCharacter(e.target.value)}
                     />
-        &nbsp;&nbsp;
-        <Button variant="outlined" size="large" startIcon={<SearchIcon />} onClick={() => onSearch(character)}>Search</Button>
     </>);
 };
