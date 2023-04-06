441263ae26aa26a281e6058a831b25a8f1d65590
Laura <laura.vidal@internaliagroup.com>
2023-04-06 19:31:08 +0200

    frameworks: angular - gallery

---
+++ 04-frameworks/angular/page/src/api/mock-gallery.ts
@@ -0,0 +1,49 @@
+import { ImageEntity } from "../app/model/ImageEntity";
+
+export const ImageList: ImageEntity[] = [
+  {
+    id: '1',
+    title: 'Gold Hoarder',
+    src: 'assets/gallery/IMG_20230326_004012.png',
+  },
+  {
+    id: '2',
+    title: 'Library',
+    src: 'assets/gallery/IMG_20230326_003838.png',
+  },
+  {
+    id: '3',
+    title: 'Map Table',
+    src: 'assets/gallery/IMG_20230326_002507.png',
+  },
+  {
+    id: '4',
+    title: 'New Golden Sands Outpost',
+    src: 'assets/gallery/IMG_20230112_002425.png',
+  },
+  {
+    id: '5',
+    title: 'Beach',
+    src: 'assets/gallery/IMG_20230108_233735.png',
+  },
+  {
+    id: '6',
+    title: 'Devils Roar',
+    src: 'assets/gallery/IMG_20230106_183735.png',
+  },
+  {
+    id: '7',
+    title: 'Captained Sloop',
+    src: 'assets/gallery/IMG_20221018_003838.png',
+  },
+  {
+    id: '8',
+    title: 'Dock',
+    src: 'assets/gallery/IMG_20220808_233855.png',
+  },
+  {
+    id: '9',
+    title: 'Glorious Sea Dogs Tavern',
+    src: 'assets/gallery/IMG_20220707_233855.png',
+  },
+];
--- 04-frameworks/angular/page/src/app/gallery/gallery.component.ts
+++ 04-frameworks/angular/page/src/app/gallery/gallery.component.ts
@@ -1,10 +1,91 @@
 import { Component } from '@angular/core';
+import { ImageEntity } from '../model/ImageEntity';
+import { ImageList } from 'src/api/mock-gallery';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
+
 export class GalleryComponent {
+
+  gallery: ImageEntity[] = [];
+  selected: ImageEntity;
+  currentKey: number;
+  length: number;
+  size: number;
+  isPlaying: boolean;
+  interval: any;
+
+  constructor() {
+    this.gallery = ImageList;
+    this.selected = this.gallery[0];
+    this.currentKey = 0;
+    this.length = this.gallery.length;
+    this.size = 1;
+    this.isPlaying = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  zoomIn(): void {
+    this.size++;
+  }
+
+  zoomOut(): void {
+    this.size--;
+  }
+
+  select(id: string): void {
+    this.size = 1;
+    this.currentKey = this.gallery.map(e => e.id).indexOf(id);
+    const obj = this.gallery.find(o => o.id === id);
+    if(typeof obj==="object") {
+      this.selected = obj;
+    } else {
+      this.selected = this.gallery[0];
+      this.currentKey=0;
+    }
+  }
+
+  handlePrev(): void{
+    this.size = 1;
+    if(this.currentKey>0) this.currentKey--;
+    else this.currentKey = this.length-1;
+    this.selected = this.gallery[this.currentKey];
+  }
+
+  handleNext(): void {
+    this.size = 1;
+    if(this.currentKey<this.length-1) this.currentKey++;
+    else  this.currentKey=0;
+    this.selected = this.gallery[this.currentKey];
+  }
+
+  previousBtn(): void {
+    if(this.currentKey>0) {
+      this.handlePrev();
+    }
+  }
+
+  nextBtn(): void {
+    if(this.currentKey<this.length-1) {
+      this.handleNext();
+    }
+  }
+
+  play(): void {
+    this.isPlaying = true;
+    this.interval = setInterval(() => {
+      this.handleNext();
+    }, 2000);
+  }
+
+  stop(): void {
+    this.isPlaying = false;
+    clearInterval(this.interval);
+  }
 
 }
